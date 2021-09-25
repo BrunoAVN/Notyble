3c302f814be42144436c1e8e0b6c0a37e9479985
Bruno Veloso <brunoavn@hotmail.com>
2021-09-25 14:40:23 -0300

    Primeiro commit

---
+++ src/sketch.js
@@ -0,0 +1,22 @@
+let svgSketch = SvgPenSketch.default;
+
+// Prep the svg element to be drawn on (custom path styles can be passed in optionally)
+var strokeStyle =  {"stroke": "red", "stroke-width": "10px"};
+const canvas = new svgSketch(document.querySelector(".sketch"), strokeStyle);
+
+canvas.getElement();
+
+
+//canvas.strokeStyle = {"stroke": "black", "stroke-width": "10px"};
+
+// Callbacks can be set for various events
+canvas.penDownCallback = (path, event) => {
+    // console.log(`Pointer location = (${event.offsetX},${event.offsetY}) @ ${event.timeStamp}`);
+};
+
+var colorPicker = new iro.ColorPicker('#picker', {width:200});
+
+function colorChange(){
+    var color = colorPicker.color.hexString;
+    canvas.strokeStyles.stroke = color;
+};
